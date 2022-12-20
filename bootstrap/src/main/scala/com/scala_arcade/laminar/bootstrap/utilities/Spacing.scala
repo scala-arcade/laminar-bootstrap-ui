5f339c196abd40e9cd4afdebd8578c5864ae9ac5
Mark Rudolph <mark@k8ty.app>
2022-12-19 22:25:44 -0500

    p and m

--- bootstrap/src/main/scala/com/scala_arcade/laminar/bootstrap/utilities/Spacing.scala
+++ bootstrap/src/main/scala/com/scala_arcade/laminar/bootstrap/utilities/Spacing.scala
@@ -6,7 +6,7 @@ import com.raquo.laminar.nodes.ReactiveHtmlElement
 import org.scalajs.dom
 
 object Spacing {
-  
+
   private val csApply: String => Setter[HtmlElement] =
     css => className := css
 
@@ -17,5 +17,77 @@ object Spacing {
   val m_4: Setter[HtmlElement]    = csApply("m-4")
   val m_5: Setter[HtmlElement]    = csApply("m-5")
   val m_auto: Setter[HtmlElement] = csApply("m-auto")
+
+  val mt_0: Setter[HtmlElement]    = csApply("mt-0")
+  val mt_1: Setter[HtmlElement]    = csApply("mt-1")
+  val mt_2: Setter[HtmlElement]    = csApply("mt-2")
+  val mt_3: Setter[HtmlElement]    = csApply("mt-3")
+  val mt_4: Setter[HtmlElement]    = csApply("mt-4")
+  val mt_5: Setter[HtmlElement]    = csApply("mt-5")
+  val mt_auto: Setter[HtmlElement] = csApply("mt-auto")
+
+  val mb_0: Setter[HtmlElement]    = csApply("mb-0")
+  val mb_1: Setter[HtmlElement]    = csApply("mb-1")
+  val mb_2: Setter[HtmlElement]    = csApply("mb-2")
+  val mb_3: Setter[HtmlElement]    = csApply("mb-3")
+  val mb_4: Setter[HtmlElement]    = csApply("mb-4")
+  val mb_5: Setter[HtmlElement]    = csApply("mb-5")
+  val mb_auto: Setter[HtmlElement] = csApply("mb-auto")
+
+  val ms_0: Setter[HtmlElement]    = csApply("ms-0")
+  val ms_1: Setter[HtmlElement]    = csApply("ms-1")
+  val ms_2: Setter[HtmlElement]    = csApply("ms-2")
+  val ms_3: Setter[HtmlElement]    = csApply("ms-3")
+  val ms_4: Setter[HtmlElement]    = csApply("ms-4")
+  val ms_5: Setter[HtmlElement]    = csApply("ms-5")
+  val ms_auto: Setter[HtmlElement] = csApply("ms-auto")
+
+  val me_0: Setter[HtmlElement]    = csApply("me-0")
+  val me_1: Setter[HtmlElement]    = csApply("me-1")
+  val me_2: Setter[HtmlElement]    = csApply("me-2")
+  val me_3: Setter[HtmlElement]    = csApply("me-3")
+  val me_4: Setter[HtmlElement]    = csApply("me-4")
+  val me_5: Setter[HtmlElement]    = csApply("me-5")
+  val me_auto: Setter[HtmlElement] = csApply("me-auto")
+
+  val p_0: Setter[HtmlElement]    = csApply("p-0")
+  val p_1: Setter[HtmlElement]    = csApply("p-1")
+  val p_2: Setter[HtmlElement]    = csApply("p-2")
+  val p_3: Setter[HtmlElement]    = csApply("p-3")
+  val p_4: Setter[HtmlElement]    = csApply("p-4")
+  val p_5: Setter[HtmlElement]    = csApply("p-5")
+  val p_auto: Setter[HtmlElement] = csApply("p-auto")
+
+  val pt_0: Setter[HtmlElement]    = csApply("pt-0")
+  val pt_1: Setter[HtmlElement]    = csApply("pt-1")
+  val pt_2: Setter[HtmlElement]    = csApply("pt-2")
+  val pt_3: Setter[HtmlElement]    = csApply("pt-3")
+  val pt_4: Setter[HtmlElement]    = csApply("pt-4")
+  val pt_5: Setter[HtmlElement]    = csApply("pt-5")
+  val pt_auto: Setter[HtmlElement] = csApply("pt-auto")
+
+  val pb_0: Setter[HtmlElement]    = csApply("pb-0")
+  val pb_1: Setter[HtmlElement]    = csApply("pb-1")
+  val pb_2: Setter[HtmlElement]    = csApply("pb-2")
+  val pb_3: Setter[HtmlElement]    = csApply("pb-3")
+  val pb_4: Setter[HtmlElement]    = csApply("pb-4")
+  val pb_5: Setter[HtmlElement]    = csApply("pb-5")
+  val pb_auto: Setter[HtmlElement] = csApply("pb-auto")
+
+  val ps_0: Setter[HtmlElement]    = csApply("ps-0")
+  val ps_1: Setter[HtmlElement]    = csApply("ps-1")
+  val ps_2: Setter[HtmlElement]    = csApply("ps-2")
+  val ps_3: Setter[HtmlElement]    = csApply("ps-3")
+  val ps_4: Setter[HtmlElement]    = csApply("ps-4")
+  val ps_5: Setter[HtmlElement]    = csApply("ps-5")
+  val ps_auto: Setter[HtmlElement] = csApply("ps-auto")
+
+  val pe_0: Setter[HtmlElement]    = csApply("pe-0")
+  val pe_1: Setter[HtmlElement]    = csApply("pe-1")
+  val pe_2: Setter[HtmlElement]    = csApply("pe-2")
+  val pe_3: Setter[HtmlElement]    = csApply("pe-3")
+  val pe_4: Setter[HtmlElement]    = csApply("pe-4")
+  val pe_5: Setter[HtmlElement]    = csApply("pe-5")
+  val pe_auto: Setter[HtmlElement] = csApply("pe-auto")
 
 }
